590b1e70165000ec2cba9bbf6e05b2d826943644
Thandi <tandeewa@gmail.com>
2020-04-24 09:48:13 -0400

    api test

--- ingred-watch/src/App.js
+++ ingred-watch/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component() {
+    constructor (props) {
+      super(props);
+      this.state = {
+        items: [],
+        isLoaded: false,
+      }
+    }
+    
+    componentDidMount (){
+      fetch('https://public.opendatasoft.com/api/records/1.0/search/?dataset=cosmetic-ingredient-database-ingredients-and-fragrance-inventory&facet=update_date&facet=restriction&facet=function')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json, 
+        })
+      })
+    }
+
+    render () {
+      var {isLoaded, items } = this.state;
+      if(!isLoaded){
+        return <div>Loading...</div>
+      }
+
+      else{
+        return (
+         <div className="App">
+           Data has loaded
+           
+           </div>
+        );
+      }
+      return (
+        <div className="App">
+
+        </div>
+      );
+    }
+    
+
 }
 
 export default App;
