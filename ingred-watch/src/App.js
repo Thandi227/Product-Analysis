c63dc7f4a9c0812f05684d7e1fc15c26dbd8aa2c
Thandi <tandeewa@gmail.com>
2020-04-24 15:56:12 -0400

    15

--- ingred-watch/src/App.js
+++ ingred-watch/src/App.js
@@ -3,9 +3,18 @@ import './App.css'
 import Search from './components/Search.js'
 import Product from './components/Product.js'
 
+
+//Here my component is being imported from the package react, this is important because it converts the class into a component. 
+
+// I Chose to use class components because if i wanted to used local state, different lifecycle hooks,etc. I could.
 class App extends Component {
-   
+
+
+
+//I'm using render here because I want to render different types of HTML elements into the DOM once the logic has been implemented 
    render (){
+
+//Within the return function there are class based child components (Search and Product) that are being rendered inside the parent component, APP
      return(
         <div classname = "App"> 
         <Search />
