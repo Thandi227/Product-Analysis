7bd9cbc66c305344fb6a8f3deeb766acc3508961
Thandi <tandeewa@gmail.com>
2020-04-24 11:01:00 -0400

    test

--- ingred-watch/src/App.js
+++ ingred-watch/src/App.js
@@ -1,48 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 class App extends Component() {
-    constructor (props) {
-      super(props);
-      this.state = {
-        items: [],
-        isLoaded: false,
-      }
-    }
-    
-    componentDidMount (){
-      fetch('https://public.opendatasoft.com/api/records/1.0/search/?dataset=cosmetic-ingredient-database-ingredients-and-fragrance-inventory&facet=update_date&facet=restriction&facet=function')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          items: json, 
-        })
-      })
-    }
-
-    render () {
-      var {isLoaded, items } = this.state;
-      if(!isLoaded){
-        return <div>Loading...</div>
-      }
-
-      else{
-        return (
-         <div className="App">
-           Data has loaded
-           
-           </div>
-        );
-      }
-      return (
-        <div className="App">
-
-        </div>
-      );
-    }
-    
-
+   render (){
+     return(
+        <div> Hello</div>
+     );
+   }
 }
 
 export default App;
